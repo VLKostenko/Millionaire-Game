5604ce9a161f243d079b13a1799a48f3eb1c49d5
vladimirkostenko <vladimirkostenko4@gmail.com>
2025-03-07 00:59:43 +0200

    reducer refactored

--- src/app/components/game-questions/game-questions.tsx
+++ src/app/components/game-questions/game-questions.tsx
@@ -17,53 +17,13 @@ import ScoreList from '@/app/components/score-list/score-list'
 // Hooks
 import useMediaQuery from '@/app/hooks/ui/useMediaQuery'
 import { useFinalScore } from '@/app/context/FinalScoreContext'
+import { quizReducer, initialState } from '@/app/modules/questions-reducer'
 
 // Interfaces
-import {
-  QuizAction,
-  QuizData,
-  QuizQuestion,
-  QuizState,
-  ScoreInterface,
-} from '@/app/game/interfaces'
+import { QuizData, QuizQuestion, ScoreInterface } from '@/app/game/interfaces'
 
-const quizData: QuizData = questions
+export const quizData: QuizData = questions
 const scoreData: ScoreInterface[] = score
-
-const initialState: QuizState = {
-  currentQuestionIndex: 0,
-  selectedAnswers: [],
-  isAnswered: false,
-  isQuizCompleted: false,
-}
-
-function quizReducer(state: QuizState, action: QuizAction): QuizState {
-  switch (action.type) {
-    case 'SELECT_ANSWER':
-      if (state.isAnswered) return state
-      return {
-        ...state,
-        selectedAnswers: state.selectedAnswers.includes(action.payload)
-          ? state.selectedAnswers.filter((ans) => ans !== action.payload)
-          : [...state.selectedAnswers, action.payload],
-      }
-    case 'CHECK_ANSWER':
-      return { ...state, isAnswered: true }
-    case 'NEXT_QUESTION':
-      if (state.currentQuestionIndex < quizData.questions.length - 1) {
-        return {
-          ...state,
-          currentQuestionIndex: state.currentQuestionIndex + 1,
-          selectedAnswers: [],
-          isAnswered: false,
-        }
-      } else {
-        return { ...state, isQuizCompleted: true }
-      }
-    default:
-      return state
-  }
-}
 
 const GameQuestions: React.FC = () => {
   const { setNewAmount } = useFinalScore()
--- src/app/modules/questions-reducer.ts
+++ src/app/modules/questions-reducer.ts
@@ -1,72 +1,40 @@
-// import {quizData} from "@/app/components/game-questions/game-questions";
-//
-// // Определяем структуру состояния
-// export interface QuizState {
-//   currentQuestionIndex: number;
-//   selectedAnswers: string[];
-//   isAnswered: boolean;
-//   isCorrect: boolean;
-//   isQuizCompleted: boolean;
-// }
-//
-// // Начальное состояние квиза
-// export const initialState: QuizState = {
-//   currentQuestionIndex: 0,
-//   selectedAnswers: [],
-//   isAnswered: false,
-//   isCorrect: false,
-//   isQuizCompleted: false,
-// };
-//
-// // Определяем типы экшенов
-// type QuizAction =
-//   | { type: 'SELECT_ANSWER'; payload: string }
-//   | { type: 'CHECK_ANSWER' }
-//   | { type: 'NEXT_QUESTION' }
-//   | { type: 'RESET_QUIZ' };
-//
-// // Редюсер
-// export const quizReducer = (state: QuizState, action: QuizAction): QuizState => {
-//   switch (action.type) {
-//     case 'SELECT_ANSWER':
-//       return {
-//         ...state,
-//         selectedAnswers: [...state.selectedAnswers, action.payload],
-//         isAnswered: true,
-//       };
-//
-//     case 'CHECK_ANSWER': {
-//       const question = quizData.questions[state.currentQuestionIndex];
-//       const correctAnswers = Array.isArray(question.correct)
-//         ? question.correct
-//         : [question.correct];
-//
-//       const isCorrect =
-//         state.selectedAnswers.length > 0 &&
-//         state.selectedAnswers.every((ans) => correctAnswers.includes(ans)) &&
-//         correctAnswers.length === state.selectedAnswers.length;
-//
-//       return { ...state, isCorrect };
-//     }
-//
-//     case 'NEXT_QUESTION': {
-//       const nextIndex = state.currentQuestionIndex + 1;
-//       const isQuizCompleted = nextIndex >= quizData.questions.length;
-//
-//       return {
-//         ...state,
-//         currentQuestionIndex: isQuizCompleted ? state.currentQuestionIndex : nextIndex,
-//         selectedAnswers: [],
-//         isAnswered: false,
-//         isCorrect: false,
-//         isQuizCompleted,
-//       };
-//     }
-//
-//     case 'RESET_QUIZ':
-//       return initialState;
-//
-//     default:
-//       return state;
-//   }
-// };
+import { quizData } from '@/app/components/game-questions/game-questions'
+import { QuizAction, QuizState } from '@/app/game/interfaces'
+
+export const initialState: QuizState = {
+  currentQuestionIndex: 0,
+  selectedAnswers: [],
+  isAnswered: false,
+  isQuizCompleted: false,
+}
+
+export const quizReducer = (
+  state: QuizState,
+  action: QuizAction
+): QuizState => {
+  switch (action.type) {
+    case 'SELECT_ANSWER':
+      if (state.isAnswered) return state
+      return {
+        ...state,
+        selectedAnswers: state.selectedAnswers.includes(action.payload)
+          ? state.selectedAnswers.filter((ans) => ans !== action.payload)
+          : [...state.selectedAnswers, action.payload],
+      }
+    case 'CHECK_ANSWER':
+      return { ...state, isAnswered: true }
+    case 'NEXT_QUESTION':
+      if (state.currentQuestionIndex < quizData.questions.length - 1) {
+        return {
+          ...state,
+          currentQuestionIndex: state.currentQuestionIndex + 1,
+          selectedAnswers: [],
+          isAnswered: false,
+        }
+      } else {
+        return { ...state, isQuizCompleted: true }
+      }
+    default:
+      return state
+  }
+}
