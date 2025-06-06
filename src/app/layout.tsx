81067705d210472a3ecf6ce72caa20c90e4a0ce1
vladimirkostenko <vladimirkostenko4@gmail.com>
2025-06-06 14:13:56 +0300

    update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import { FinalScoreProvider } from '@/context/FinalScoreContext';
 
 const inter = Inter({
   variable: '--font-inter-sans',
-  weight: ['400', '600', '800'],
+  weight: ['400', '600', '800', '900'],
   style: ['normal'],
   subsets: ['latin'],
   display: 'swap',
